33514e0d7ddd3ecee644e77abf18f5da6b820ed5
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-10 19:16:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# w1-d4-exercise-js-switch-thrones
+
+1. Use the same technique to add the Lannister house to our script.
+2. Don’t forget to add the break statement to avoid conflicts.
---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Ready to go! 🚀");
+
+// const name = prompt("Favorite Game of Thrones main character:");
+// let house = "";
+
+// switch (name) {
+//   case "Khal Drogo":
+//     house = "Dothraki Horselord";
+//     break;
+//   case "Daenerys":
+//     house = "Targaryen";
+//     break;
+//   case "Jon Snow":
+//   case "Sansa":
+//   case "Arya":
+//     house = "Stark";
+//     break;
+// }
+
+// console.log(`Your favorite character is from the house ${house}.`);
